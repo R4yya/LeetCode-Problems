cd879989ce277facfd9954b54ee57154db189718
R4yya <121268707+R4yya@users.noreply.github.com>
2023-04-15 03:22:29 +0300

    Delete 27_Remove Element.py

--- src/27_Remove Element.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        for i in range(0, nums.count(val)):
-            nums.remove(val)
-
-        return len(nums)
