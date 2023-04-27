4e291f3c37fd10086c5f987d5a0fbde245ba118d
R4yya <121268707+R4yya@users.noreply.github.com>
2023-04-27 18:50:55 +0300

    Update 125_Valid_Palindrome.py

--- src/125_Valid_Palindrome.py
+++ src/125_Valid_Palindrome.py
@@ -1,2 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = [char for char in s.lower() if char.isalnum()]
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
